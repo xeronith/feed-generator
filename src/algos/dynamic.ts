9c771cfa95980769d3c8ef668653afce0340b9dd
Xeronith <xeronith@yahoo.com>
2024-09-10 09:47:15 +0330

    fix: resolve bigquery timestamp issue

--- src/algos/dynamic.ts
+++ src/algos/dynamic.ts
@@ -95,7 +95,7 @@ export const handler = async (ctx: AppContext, params: QueryParams) => {
     let cursor: string | undefined
     const last = res.at(-1)
     if (last) {
-      cursor = new Date(last.indexedAt).getTime().toString(10)
+      cursor = new Date(last.indexedAt.value).getTime().toString(10)
     }
 
     return {
