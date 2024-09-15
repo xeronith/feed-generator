6e0015065bcfe898cb4f1fad850a9e660d87ad91
Xeronith <xeronith@yahoo.com>
2024-09-15 18:34:04 +0330

    perf: implement storage efficient multi-layer cache

--- src/algos/dynamic.ts
+++ src/algos/dynamic.ts
@@ -1,10 +1,16 @@
 import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import { BigQuery } from '@google-cloud/bigquery'
 import { AppContext } from '../config'
+import * as LZString from 'lz-string'
 import path from 'path'
 
 // max 15 chars
 export const shortname = 'dynamic'
+
+const inProcCache: Record<
+  string,
+  { content: { uri: string; indexedAt: string }[]; refreshedAt: string }
+> = {}
 
 export const handler = async (ctx: AppContext, params: QueryParams) => {
   const identifier = params[shortname]
@@ -59,89 +65,105 @@ export const handler = async (ctx: AppContext, params: QueryParams) => {
       new Date().getTime() - ctx.cfg.cacheTimeout,
     ).toISOString()
 
-    let cache = await ctx.db
-      .selectFrom('cache')
-      .selectAll()
-      .where('identifier', '=', identifier)
-      .where('refreshedAt', '>', cacheTimeout)
-      .executeTakeFirst()
-
-    if (!cache) {
-      let query = `SELECT \`uri\`, \`indexedAt\` FROM \`${ctx.cfg.bigQueryDatasetId}.${ctx.cfg.bigQueryTableId}\` WHERE`
-
-      query +=
-        ' `indexedAt` > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 7 DAY)'
-
-      if (params.cursor) {
-        const timeStr = new Date(parseInt(params.cursor, 10)).toISOString()
-        query += ` AND \`indexedAt\` < '${timeStr}'`
-      }
-
-      const parameters: any[] = []
-
-      if (
-        authors.length === 0 &&
-        hashtags.length === 0 &&
-        mentions.length === 0 &&
-        search.length === 0
-      ) {
-        query += ` AND \`author\` = 'UNKNOWN'`
-      } else {
-        if (authors.length > 0) {
-          authors.forEach((author) => parameters.push(author))
-          const authorList = authors.map(() => '?').join(', ')
-          query += ` AND \`author\` IN (${authorList})`
-        }
-
-        hashtags.forEach((hashtag) => {
-          parameters.push(hashtag.replace('-', ' '))
-          query += ` AND SEARCH(\`text\`, ?)`
-        })
-
-        mentions.forEach((mention) => {
-          parameters.push(mention.replace('-', ' '))
-          query += ` AND SEARCH(\`text\`, ?)`
-        })
-
-        search.forEach((criteria) => {
-          parameters.push(criteria.replace('-', ' '))
-          query += ` AND SEARCH(\`text\`, ?)`
-        })
-      }
-
-      query += ` ORDER BY \`indexedAt\` DESC, \`uri\` DESC;`
-
-      const [res] = await new BigQuery({
-        projectId: ctx.cfg.bigQueryProjectId,
-        keyFilename: path.resolve(__dirname, '..', ctx.cfg.bigQueryKeyFile),
-      }).query({
-        query: query,
-        params: parameters,
-      })
-
-      ctx.db
-        .insertInto('cache')
-        .values({
-          identifier: identifier,
-          content: JSON.stringify(res),
-          refreshedAt: new Date().toISOString(),
-        })
-        .onConflict((e) =>
-          e.doUpdateSet({
-            content: JSON.stringify(res),
-            refreshedAt: new Date().toISOString(),
-          }),
-        )
-        .execute()
-
-      cache = await ctx.db
+    if (
+      !inProcCache[identifier] ||
+      inProcCache[identifier].refreshedAt < cacheTimeout
+    ) {
+      let cache = await ctx.db
         .selectFrom('cache')
         .selectAll()
         .where('identifier', '=', identifier)
+        .where('refreshedAt', '>', cacheTimeout)
         .executeTakeFirst()
+
+      if (!cache) {
+        let query = `SELECT \`uri\`, \`indexedAt\` FROM \`${ctx.cfg.bigQueryDatasetId}.${ctx.cfg.bigQueryTableId}\` WHERE`
+
+        query +=
+          ' `indexedAt` > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 7 DAY)'
+
+        if (params.cursor) {
+          const timeStr = new Date(parseInt(params.cursor, 10)).toISOString()
+          query += ` AND \`indexedAt\` < '${timeStr}'`
+        }
+
+        const parameters: any[] = []
+
+        if (
+          authors.length === 0 &&
+          hashtags.length === 0 &&
+          mentions.length === 0 &&
+          search.length === 0
+        ) {
+          query += ` AND \`author\` = 'UNKNOWN'`
+        } else {
+          if (authors.length > 0) {
+            authors.forEach((author) => parameters.push(author))
+            const authorList = authors.map(() => '?').join(', ')
+            query += ` AND \`author\` IN (${authorList})`
+          }
+
+          hashtags.forEach((hashtag) => {
+            parameters.push(hashtag.replace('-', ' '))
+            query += ` AND SEARCH(\`text\`, ?)`
+          })
+
+          mentions.forEach((mention) => {
+            parameters.push(mention.replace('-', ' '))
+            query += ` AND SEARCH(\`text\`, ?)`
+          })
+
+          search.forEach((criteria) => {
+            parameters.push(criteria.replace('-', ' '))
+            query += ` AND SEARCH(\`text\`, ?)`
+          })
+        }
+
+        query += ` ORDER BY \`indexedAt\` DESC, \`uri\` DESC;`
+
+        const [queryResult] = await new BigQuery({
+          projectId: ctx.cfg.bigQueryProjectId,
+          keyFilename: path.resolve(__dirname, '..', ctx.cfg.bigQueryKeyFile),
+        }).query({
+          query: query,
+          params: parameters,
+        })
+
+        const cacheItem = {
+          identifier: identifier,
+          content: LZString.compressToUTF16(JSON.stringify(queryResult ?? [])),
+          refreshedAt: new Date().toISOString(),
+        }
+
+        ctx.db
+          .insertInto('cache')
+          .values(cacheItem)
+          .onConflict((e) =>
+            e.doUpdateSet({
+              content: cacheItem.content,
+              refreshedAt: cacheItem.refreshedAt,
+            }),
+          )
+          .execute()
+
+        cache = await ctx.db
+          .selectFrom('cache')
+          .selectAll()
+          .where('identifier', '=', identifier)
+          .executeTakeFirst()
+      }
+
+      inProcCache[identifier] = {
+        content: JSON.parse(
+          cache ? LZString.decompressFromUTF16(cache.content) : '[]',
+        ),
+
+        refreshedAt: cache?.refreshedAt ?? new Date().toISOString(),
+      }
     }
 
-    let result: any[] = JSON.parse(cache ? cache.content : '[]')
+    let result: any[] = inProcCache[identifier].content
+
     if (params.cursor) {
       const timeStr = new Date(parseInt(params.cursor, 10)).toISOString()
       result = result.filter((row) => row.indexedAt < timeStr)
@@ -149,8 +171,8 @@ export const handler = async (ctx: AppContext, params: QueryParams) => {
 
     result = result.slice(0, params.limit)
 
-    const feed = result.map((row) => ({
-      post: row.uri,
+    const feed = result.map((item) => ({
+      post: item.uri,
     }))
 
     let cursor: string | undefined
