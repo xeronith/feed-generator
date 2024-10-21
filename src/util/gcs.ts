2241f745ea7928786a11c637dd6d0d8b0a76e14e
Xeronith <xeronith@yahoo.com>
2024-10-21 16:20:26 +0330

    feat: improve error handling

--- src/util/gcs.ts
+++ src/util/gcs.ts
@@ -4,9 +4,13 @@ export const removeFileFromStorage = async (
   ctx: AppContext,
   fileUrl: string,
 ) => {
-  const url = new URL(fileUrl)
-  const filePath = url.pathname.substring(url.pathname.indexOf('/', 1))
-  await ctx.storage.bucket(ctx.cfg.gcsBucket).file(filePath).delete({
-    ignoreNotFound: true,
-  })
+  try {
+    const url = new URL(fileUrl)
+    const filePath = url.pathname.substring(url.pathname.indexOf('/', 1))
+    await ctx.storage.bucket(ctx.cfg.gcsBucket).file(filePath).delete({
+      ignoreNotFound: true,
+    })
+  } catch (error) {
+    console.debug('GCS', error.message)
+  }
 }
