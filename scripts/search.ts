218a0a0ea1663716924773f61a6877344af55bc7
Xeronith <xeronith@yahoo.com>
2025-01-27 09:46:19 +0330

    chore: update scripts

---
+++ scripts/search.ts
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv'
+import { AtpAgent } from '@atproto/api'
+
+const run = async () => {
+  dotenv.config()
+
+  if (!process.env.FEEDGEN_PUBLISH_HANDLE) {
+    throw new Error('Please provide a handle in the .env file')
+  }
+
+  if (!process.env.FEEDGEN_PUBLISH_APP_PASSWORD) {
+    throw new Error('Please provide an app password in the .env file')
+  }
+
+  const handle = process.env.FEEDGEN_PUBLISH_HANDLE,
+    password = process.env.FEEDGEN_PUBLISH_APP_PASSWORD
+
+  let query = 'BlueSky'
+  if (process.argv[2]) {
+    query = process.argv[2]
+  }
+
+  // only update this if in a test environment
+  const agent = new AtpAgent({ service: 'https://bsky.social' })
+
+  await agent.login({
+    identifier: handle,
+    password: password,
+  })
+
+  const result = await agent.api.app.bsky.feed.searchPosts({
+    q: query,
+  })
+
+  console.log(result.data)
+}
+
+run()
